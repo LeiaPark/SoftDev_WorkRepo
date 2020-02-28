525df0906e52467d4dadb59a050d91d60724558a
kimi <kimi@kimis-MacBook-Air.local>
2020-02-27 20:21:57 -0500

    20200227

--- 08_canvas-anim/dvd.js
+++ 08_canvas-anim/dvd.js
@@ -48,3 +48,35 @@ buttonStart.addEventListener('click', startAnim);
 
 var buttonStop = document.getElementById("stop");
 buttonStop.addEventListener('click', stopAnim);
+
+
+var radius = 5;
+var change = 1;
+var id = 0;
+
+
+function clear(e) {
+	ctx.clearRect(0, 0, 500, 500);
+}
+
+function startAnim2() {
+	window.cancelAnimationFrame(id);
+	if (radius == 250) {
+		change = -1;
+	}
+	if (radius == 0) {
+		change = 1;
+	}
+	clear();
+	ctx.beginPath();
+	ctx.arc(250, 250, radius += change, 0, 2 * Math.PI);
+	ctx.fillStyle = "#f5df04";
+	ctx.fill();
+	id = window.requestAnimationFrame(startAnim2);
+};
+
+
+var buttonStart = document.getElementById("start");
+buttonStart.addEventListener('click', function(e) {
+	id = window.requestAnimationFrame(startAnim2);
+});
--- 09_mongo/app.py
+++ 09_mongo/app.py
@@ -1,35 +1,62 @@
+#Leia Park & Connor Oh
+#SoftDev1 pd9
+#K09 -- Yummy Mongo Py
+#2020-02-28
+
+from bson.json_util import loads
 from pymongo import MongoClient
 
 client = MongoClient('localhost', 27017)
 db = client['test']
-collection = db['restaurants']
+collection = db.restaurants #creates a collection for the restaurants
 
+#read in data============================
+if(collection.count()==0):
+    file = open("primer-dataset.json", "r")
+    content = file.readlines()
+    for line in content:
+        collection.insert_one(loads(line))
+        
 # Specified borough
 def getBorough(borough):
      data = collection.find({"borough": borough})
      for item in data:
-          return item
+        for key, value in item.items():
+            if key == "name":
+                print("{name: %s}" % value)
 
-# Specified zip code
+
+# Specified zip codefor item in data:
+          return item
 def getZipCode(zipcode):
      data = collection.find({"address.zipcode": zipcode})
      for item in data:
-          return item
+        for key, value in item.items():
+            if key == "name":
+                print("{name: %s}" % value)
 
 # Specified zip code & grade
 def getZipGrade(zipcode, grade):
      data = collection.find({"address.zipcode": zipcode, "grades.0.grade": grade})
      for item in data:
-          return item
+        for key, value in item.items():
+            if key == "name":
+                print("{name: %s}" % value)
 
 # Specified zip code w/ score below a threshold
 def getZipScore(zipcode, score):
      data = collection.find({"address.zipcode": zipcode, "grades.0.grade": {"$lt": score}})
      for item in data:
-          return item
+        for key, value in item.items():
+            if key == "name":
+                print("{name: %s}" % value)
 
 # Something more clever
 def getCuisine(cuisine):
      data = collection.find({"cuisine": cuisine})
      for item in data:
-          return item
+        for key, value in item.items():
+            if key == "name":
+                print("{name: %s}" % value)
+
+print(getZipCode("11357"))
